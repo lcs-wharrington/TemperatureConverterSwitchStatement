b7942ed2dcd1483723669dbb596a5b0326ff9104
William Harrington <wharrington@lcs.on.ca>
2021-12-03 08:40:45 -0500

    Initial Commit

---
+++ TemperatureConverterSwitchStatement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureConverterSwitchStatement
+//
+//  Created by William Robert Harrington on 2021-12-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TemperatureConverterSwitchStatement/TemperatureConverterSwitchStatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterSwitchStatementApp.swift
+//  TemperatureConverterSwitchStatement
+//
+//  Created by William Robert Harrington on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterSwitchStatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
